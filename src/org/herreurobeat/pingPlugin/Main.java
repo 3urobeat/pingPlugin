edd11ed5bb0fdbedd0dcddc2fca70046ede78f89
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-12-08 16:28:51 +0100

    Added support for targeting other players and improved code structure

--- src/org/herreurobeat/pingPlugin/Main.java
+++ src/org/herreurobeat/pingPlugin/Main.java
@@ -1,5 +1,6 @@
 package org.herreurobeat.pingPlugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.bukkit.GameRule;
@@ -9,6 +10,7 @@ import org.bukkit.entity.Player;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
 
 public class Main extends JavaPlugin {
 
@@ -69,40 +71,97 @@ public class Main extends JavaPlugin {
      * @param args The arguments the user provided
      */
     public void pingCommand(CommandSender sender, String[] args) {
+
+        //Check if we are allowed to respond
         if (sender instanceof Player) {
             Player p = (Player) sender;
-            if (!p.getWorld().getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK))
-                return; //check gamerule and stop if send command feedback is false
+
+            if (!p.getWorld().getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK)) return; //check gamerule and stop if send command feedback is false
+
         }
 
 
-        try {
+        //Check which player to get ping of
+        Player targetPlayer = getTargetPlayer(sender, args);
 
-            Player player = (Player) sender;
+        if (targetPlayer == null) return;
+
+
+        //Get ping of targetPlayer
+        int ping = getPing(targetPlayer);
+
+        if (ping < 0) return;
+
+
+        //Respond with Your or targetPlayer name
+        if (targetPlayer.getName().equals(sender.getName())) {
+            sender.sendMessage("§aYour ping: §e" + ping + "ms");
+        } else {
+            sender.sendMessage("§a" + targetPlayer.getName() + "'s ping: §e" + ping + "ms");
+        }
+
+    }
+
+
+
+    /**
+     * Get the player we want to target
+     * @param sender The player who used the command
+     * @param args The arguments the user provided
+     */
+    private Player getTargetPlayer(CommandSender sender, String[] args) {
+        if (args.length == 1) { //Player name provided?
+
+            if (Bukkit.getPlayer(args[0]) != null) { //check if provided player name is online, otherwise it will throw an error
+                return Bukkit.getPlayer(args[0]);
+            } else {
+                sender.sendMessage("That player doesn't seem to be online!");
+                return null;
+            }
+
+        } else if (sender instanceof Player) { //No name provided, the targeted player is the sender
+
+            return (Player) sender;
+
+        } else {
+
+            sender.sendMessage("Only Players can check their own ping!");
+            return null;
+        }
+    }
+
+
+    /**
+     * Gets the ping of the targetPlayer
+     * @param targetPlayer The targeted player
+     * @return The ping in ms
+     */
+    private int getPing(Player targetPlayer) {
+        try {
 
             //Check which version the server uses. 1.17 and above have a getPing() method, otherwise we need to access the CraftPlayer Class which has a different path each version
             if (serverVersion >= 117) {
 
-                int ping = player.getPing();
-                sender.sendMessage("§aYour ping: §e" + ping + "ms");
+                return targetPlayer.getPing();
 
             } else {
 
                 //Get CraftPlayer class dynamically based on which version the server is running on
                 Class<?> CraftPlayerClass = Class.forName(getServer().getClass().getPackage().getName() + ".entity.CraftPlayer"); //get class
-                Object CraftPlayer        = CraftPlayerClass.cast(player); //make new object
+                Object CraftPlayer        = CraftPlayerClass.cast(targetPlayer); //make new object
 
                 Method getHandle = CraftPlayer.getClass().getMethod("getHandle"); //get getHandle method from CraftPlayer
 
                 Object EntityPlayer = getHandle.invoke(CraftPlayer); //make new EntityPlayer with getHandle() method
                 Field  ping         = EntityPlayer.getClass().getDeclaredField("ping"); //get field ping from EntityPlayer
 
-                sender.sendMessage("§aYour ping: §e" + ping.getInt(EntityPlayer) + "ms");
+                return ping.getInt(EntityPlayer);
 
             }
 
         } catch (Exception err) {
-            sender.sendMessage("Error: " + err.getMessage());
+            targetPlayer.sendMessage("Error: " + err.getMessage());
+            return -1;
         }
     }
 }
