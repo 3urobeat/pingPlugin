4bc3228192a34bf1ca754ed2f09e0df20f5f3335
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-12-08 15:33:52 +0100

    Added support for 1.17+ and fixed plugin only working on 1.16

--- src/org/herreurobeat/pingPlugin/Main.java
+++ src/org/herreurobeat/pingPlugin/Main.java
@@ -1,7 +1,5 @@
 package org.herreurobeat.pingPlugin;
 
-import net.minecraft.server.v1_16_R3.EntityPlayer;
-import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.bukkit.GameRule;
@@ -9,14 +7,25 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Main extends JavaPlugin {
 
     String supportedVersion = "1.16";
     String pluginVersion = "1.0";
 
+    int serverVersion;
+
     @Override
     public void onEnable() {
         System.out.println("pingPlugin for version " + supportedVersion + " enabled!");
+
+        //Get server version once on startup to reduce load when using command
+        String serverVersionStr   = getServer().getBukkitVersion();
+        String serverVersionMajor = serverVersionStr.split("\\.")[0] + serverVersionStr.split("\\.")[1].split("-")[0]; //get major mc version
+
+        serverVersion = Integer.parseInt(serverVersionMajor.replaceAll("\\.", "")); //convert to number to make comparable
     }
 
     //Handle command execution
@@ -66,7 +75,34 @@ public class Main extends JavaPlugin {
                 return; //check gamerule and stop if send command feedback is false
         }
 
-        EntityPlayer player = ((CraftPlayer) sender).getHandle();
-        sender.sendMessage("§aYour ping: §e" + player.ping + "ms");
+
+        try {
+
+            Player player = (Player) sender;
+
+            //Check which version the server uses. 1.17 and above have a getPing() method, otherwise we need to access the CraftPlayer Class which has a different path each version
+            if (serverVersion >= 117) {
+
+                int ping = player.getPing();
+                sender.sendMessage("§aYour ping: §e" + ping + "ms");
+
+            } else {
+
+                //Get CraftPlayer class dynamically based on which version the server is running on
+                Class<?> CraftPlayerClass = Class.forName(getServer().getClass().getPackage().getName() + ".entity.CraftPlayer"); //get class
+                Object CraftPlayer        = CraftPlayerClass.cast(player); //make new object
+
+                Method getHandle = CraftPlayer.getClass().getMethod("getHandle"); //get getHandle method from CraftPlayer
+
+                Object EntityPlayer = getHandle.invoke(CraftPlayer); //make new EntityPlayer with getHandle() method
+                Field  ping         = EntityPlayer.getClass().getDeclaredField("ping"); //get field ping from EntityPlayer
+
+                sender.sendMessage("§aYour ping: §e" + ping.getInt(EntityPlayer) + "ms");
+
+            }
+
+        } catch (Exception err) {
+            sender.sendMessage("Error: " + err.getMessage());
+        }
     }
 }
